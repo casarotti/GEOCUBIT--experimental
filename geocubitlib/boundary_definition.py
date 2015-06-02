cb7b8b771a0073a083bbf930ac358f9022e1b189
Emanuele Casarotti <casarotti@tiscali.it>
2015-06-02 17:09:26 +0200

    improving for single chunk

--- geocubitlib/boundary_definition.py
+++ geocubitlib/boundary_definition.py
@@ -342,7 +342,7 @@ def define_bc(*args,**keys):
         v_list,name_list=define_block()
         build_block(v_list,name_list,id_0,top_surf,optionsea=optionsea)
         # entities
-        if type(['e'])==list:
+        if type(args)==list:
             entities=args[0]
         else:
             entities='face'
